0d5145addb6e57ea0d207897fad6da8856c0b381
Nishant0706 <118384655+Nishant0706@users.noreply.github.com>
2025-09-27 23:00:21 +0530

    Add files via upload

---
+++ task 2.py
@@ -0,0 +1,7 @@
+a=input("Enter your first name: ")
+b=input("Enter your last name: ")
+
+c = a + " " + b
+
+# Step 3: Print a personalized greeting message using the full name
+print(f"Hello, {c}! Welcome to the program.")
